4ef8b2d5316353b2cfb4e2a0908bf8497a4fb6a5
georgegoldman <georgegoldman2014@gmail.com>
2021-09-27 19:07:07 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    int x;
+    int maxSpeedValue;
+    public Main(int y, int speed)
+    {
+        x = y;
+        maxSpeedValue = speed;
+    }
+    
+    public static void main (String[] args)
+    {
+        Main firstMainObj = new Main(5, 200);
+        Main secondMainbObj = new Main(25, 200);
+        System.out.println(firstMainObj.x);
+        System.out.println(secondMainbObj.x);
+
+        myStaticMethod();
+        // Main tryingTocallANonstaticMethod = new Main();
+        // tryingTocallANonstaticMethod.nonstaticMethod();
+    }
+
+    public void fullThrottle(){
+        System.out.println("The car is going as fast as it can!");
+    }
+
+    public void speed(int maxSpeed) {
+        System.out.print("Max speed is: "+  maxSpeed + "\n");
+    }
+
+    static void myStaticMethod() 
+    {
+        System.out.println("static method can be called without creating objects");
+    }
+
+    public void nonstaticMethod()
+    {
+        System.out.println("Public methods must be called by creating an object.");
+    }
+
+}
---
+++ src/Second.java
@@ -0,0 +1,8 @@
+public class Second {
+    public static void main(String[] args) 
+    {
+        Main myCar = new Main(5, 200);
+        myCar.fullThrottle();
+        myCar.speed(200);
+    }
+}
